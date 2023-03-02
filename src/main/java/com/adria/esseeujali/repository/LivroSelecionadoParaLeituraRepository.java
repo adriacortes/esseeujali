4300476872e38fd159a6bae0388483df743ddfeb
Ádria Aline Castro da Silva Côrtes <adria.aline@gmail.com>
2023-03-02 15:50:16 -0300

    List<RankingPontuacao> buscarPontosTodosUsuarios() esta funcionando o inner join.

--- src/main/java/com/adria/esseeujali/repository/LivroSelecionadoParaLeituraRepository.java
+++ src/main/java/com/adria/esseeujali/repository/LivroSelecionadoParaLeituraRepository.java
@@ -2,6 +2,7 @@ package com.adria.esseeujali.repository;
 
 import com.adria.esseeujali.model.LivroSelecionadoPK;
 import com.adria.esseeujali.model.LivroSelecionadoParaLeitura;
+import com.adria.esseeujali.model.RankingPontuacao;
 import com.adria.esseeujali.model.Trofeu;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -16,7 +17,10 @@ public interface LivroSelecionadoParaLeituraRepository extends JpaRepository<Liv
             " GROUP BY li.genero HAVING COUNT(li) >= 5 "
     )
     List<Trofeu> buscarTrofeuDoUsuario(int id);
-    
+
     @Query(value = "SELECT sum(pontuacao) FROM livro_selecionado_para_leitura ls WHERE ls.usuario_id= :id", nativeQuery = true)
     Integer buscarPontosUsuario(int id);
+
+    @Query("SELECT us.nome FROM LivroSelecionadoParaLeitura ls INNER JOIN Usuario us ON ls.livroSelecionadoPk.usuarioId = us.id")
+    List<RankingPontuacao> buscarPontosTodosUsuarios();
 }
