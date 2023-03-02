ddc461939d86dd4fa32f92195928499ddfba009c
Ádria Aline Castro da Silva Côrtes <adria.aline@gmail.com>
2023-03-02 10:58:59 -0300

    Meto buscarTrofeuDoUsuario com Inner Join funcionando.

--- src/main/java/com/adria/esseeujali/repository/LivroSelecionadoParaLeituraRepository.java
+++ src/main/java/com/adria/esseeujali/repository/LivroSelecionadoParaLeituraRepository.java
@@ -2,7 +2,11 @@ package com.adria.esseeujali.repository;
 
 import com.adria.esseeujali.model.LivroSelecionadoPK;
 import com.adria.esseeujali.model.LivroSelecionadoParaLeitura;
+import com.adria.esseeujali.model.Trofeu;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface LivroSelecionadoParaLeituraRepository extends JpaRepository<LivroSelecionadoParaLeitura, LivroSelecionadoPK> {
 
@@ -10,7 +14,7 @@ public interface LivroSelecionadoParaLeituraRepository extends JpaRepository<Liv
 //            "where ll.id_usuario=:id" +
 //            " group by l.genero having total > 5",nativeQuery = true)
 
-    //    @Query("SELECT l.genero,count(*) as total FROM LivroSelecionadoParaLeitura ll  join Livro l ON ll.livroSelecionadoPk.livroId = l.id" +
-//            " where Usuario.id=:id")
-    //List<Trofeu> buscarTrofeuDoUsuario(int id);
+    @Query("SELECT l FROM LivroSelecionadoParaLeitura l INNER JOIN Livro li ON l.livroSelecionadoPk.livroId = li.id")
+    List<Trofeu> buscarTrofeuDoUsuario(int id);
+
 }
